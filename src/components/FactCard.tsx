bd0eddb4f7d9f915ffd0c117ddde6d3b1856e54d
Paolo Debuque <pdebuque@gmail.com>
2023-05-20 12:16:06 -0500

    about page fancy styling

--- src/components/FactCard.tsx
+++ src/components/FactCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as styles from '../styles/factcard.module.css'
 
@@ -10,18 +10,28 @@ type Props = {
 
 const FactCard: React.FC<Props> = (props) => {
 
+  const [hover, setHover] = useState<boolean>(false)
+
   const {
     fact
   } = props
 
   return (
-    <div className={styles.card}>
-      <div className = {styles.header}>
-        <p className = {styles.emoji}>{fact.emoji1}</p>
+    <div
+      className={`${styles.card} ${hover && styles.cardHover}`}
+      onMouseEnter={() => {
+        console.log('hover')
+        setHover(true)
+      }
+      }
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className={styles.header}>
+        <p className={`${styles.emoji} ${hover && styles.emojiHover}`}>{fact.emoji1}</p>
         <h1 className={styles.title}>
           {fact.title}
         </h1>
-        <p className = {styles.emoji}>{fact.emoji2}</p>
+        <p className={`${styles.emoji} ${hover && styles.emojiHover}`}>{fact.emoji2}</p>
       </div>
       <p dangerouslySetInnerHTML={{ __html: fact.text }} className={styles.text}></p>
       {/* <div className = {styles.footer}>
