27c301ad7b22c55632eda719081da609a44bd788
Paolo Debuque <pdebuque@gmail.com>
2023-05-22 15:56:56 -0500

    fix metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Paolo Debuque, developer`,
+    description: `Paolo Debuque's personal dev portfolio`,
+    author: `Paolo Debuque`,
+    siteUrl: `https://paolodebuque.dev`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
